81919a47641dff329b502e0d9cae334b936d960a
adi <acpr@kth.se>
2016-09-16 13:54:10 +0200

    added travel and purpose schema definitions in apiv2

--- SQL/v2_api.sql
+++ SQL/v2_api.sql
@@ -18,14 +18,14 @@ CREATE TABLE IF NOT EXISTS apiv2.trips_inf
   length_of_trip double precision,
   duration_of_trip double precision,
   number_of_triplegs integer,
-  trip_id serial NOT NULL
+  trip_id serial NOT NULL primary key
 );
 COMMENT ON TABLE apiv2.trips_inf is 
 'Stores the trips that have been detected by the segmentation algorithm';
 
 CREATE TABLE IF NOT EXISTS apiv2.triplegs_inf
 (
-  user_id int references raw.user_table(id),
+  user_id int references raw_data.user_table(id),
   from_point_id bigint,
   to_point_id bigint,
   from_time bigint,
@@ -35,17 +35,17 @@ CREATE TABLE IF NOT EXISTS apiv2.triplegs_inf
   transition_poi_id bigint,
   length_of_tripleg double precision,
   duration_of_tripleg double precision,
-  tripleg_id serial NOT NULL,
+  tripleg_id serial NOT NULL primary key,
   trip_id int references apiv2.trips_inf(trip_id)
   );
 COMMENT ON TABLE apiv2.triplegs_inf is 
 'Stores the triplegs that have been detected by the segmentation algorithm';
 
-CREATE TABLE apiv2.trips_gt
+CREATE TABLE IF NOT EXISTS apiv2.trips_gt
 (
-  trip_id serial not null,
+  trip_id serial not null primary key,
   trip_inf_id int references apiv2.trips_inf(trip_id),
-  user_id bigint references raw.user_table(id),
+  user_id bigint references raw_data.user_table(id),
   from_point_id text,
   to_point_id text,
   from_time bigint,
@@ -60,10 +60,10 @@ CREATE TABLE apiv2.trips_gt
 COMMENT ON TABLE apiv2.trips_gt is 
 'Stores the trips that have been annotated by the user';
 
-CREATE TABLE apiv2.triplegs_gt
+CREATE TABLE IF NOT EXISTS apiv2.triplegs_gt
 (
   tripleg_id serial not null,
-  tripleg_inf_id int references apiv2.triplegs_inf(tripleg_id)
+  tripleg_inf_id int references apiv2.triplegs_inf(tripleg_id),
   trip_id int references apiv2.trips_gt(trip_id),
   user_id bigint,
   from_point_id text,
@@ -80,12 +80,66 @@ CREATE TABLE apiv2.triplegs_gt
 COMMENT ON TABLE apiv2.triplegs_gt is 
 'Stores the triplegs that have been annotated by the user';
 
+CREATE EXTENSION IF NOT EXISTS postgis; 
+
+CREATE TABLE IF NOT EXISTS apiv2.pois
+(
+  gid serial NOT NULL primary key,
+  type_ text,
+  name_ text,
+  lat_ double precision,
+  lon_ double precision,
+  user_id bigint,
+  osm_id bigint default 0,
+  is_personal boolean,
+  geom geometry(Point,3006)
+);
+
+COMMENT ON TABLE apiv2.pois is 
+'Stores the POIs used by the app';
+
+CREATE TABLE IF NOT EXISTS apiv2.poi_transportation
+(
+gid serial NOT NULL primary key,
+  osm_id bigint default 0,
+  type_ text,
+  name_ text,
+  lat_ double precision,
+  lon_ double precision,
+  declared_by_user boolean DEFAULT false,
+  transportation_lines text,
+  transportation_types text,
+  declaring_user_id integer,
+  type_sv text,
+  geom geometry(Point,3006)
+);
+COMMENT ON TABLE apiv2.poi_transportation is 
+'Stores the transportation POIs used by the app';
+
+CREATE TABLE IF NOT EXISTS apiv2.purpose_table
+(
+  id smallint,
+  name_ text,
+  name_sv text
+);
+COMMENT ON TABLE apiv2.purpose_table is 
+'Stores the purpose schema';
+
+CREATE TABLE IF NOT EXISTS apiv2.travel_mode_table
+(
+  id smallint,
+  name_ text,
+  name_sv text
+);
+COMMENT ON TABLE apiv2.travel_mode_table is 
+'Stores the travel mode schema';
+
 /*
 Unannotated trips and triplegs for pagination 
 */
 
-create view apiv2.unprocessed_trips as
-        select * from trips_inf as ti where
+create or replace view apiv2.unprocessed_trips as
+        select * from apiv2.trips_inf as ti where
         from_time>= (
         select max(tg.to_time) from apiv2.trips_gt tg where
         tg.user_id = ti.user_id
@@ -94,7 +148,7 @@ create view apiv2.unprocessed_trips as
 Comment on view apiv2.unprocessed_trips is 
 'Used to serve the unannotated trips to the user on the pagination - selection by user_id';
 
-create view apiv2.unprocessed_triplegs as
+create or replace view apiv2.unprocessed_triplegs as
         select * from apiv2.triplegs_inf where trip_id =
         any (select trip_id from apiv2.unprocessed_trips);
 Comment on view apiv2.unprocessed_trips is 
@@ -104,13 +158,13 @@ Comment on view apiv2.unprocessed_trips is
 Annotated trips and triplegs annotation 
 */
 -- view that will serve the annotated trips to the user on request
-create view apiv2.processed_trips as
+create or replace view apiv2.processed_trips as
         select * from apiv2.trips_gt where type_of_trip = 1;
 Comment on view apiv2.unprocessed_trips is 
 'Used to serve the annotated trips of a user - selection per user_id';
 
 -- view that will serve the annotated triplegs per trip
-create view apiv2.processed_triplegs as
+create or replace view apiv2.processed_triplegs as
         select * from apiv2.triplegs_inf where trip_id =
         any (select trip_id from apiv2.processed_trips); 
         Comment on view apiv2.unprocessed_trips is 
