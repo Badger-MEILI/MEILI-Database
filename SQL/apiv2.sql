d91dc5f6d1db59a5fd4d3462a7d419b5b24069c1
adi <acpr@kth.se>
2016-09-16 08:46:42 +0200

    split the raw data in its own schema, hashed user passwords and implemented case insensitive user ids

--- SQL/apiv2.sql
+++ SQL/apiv2.sql
@@ -19,6 +19,45 @@ create view v2api.unprocessed_triplegs as
 	select * from triplegs_inf where trip_id = 
 	any (select trip_id from v2api.unprocessed_trips);
 
+-- paginates to the first unannotated trip that the user has to interact with
+
+create function v2api.pagination_get_next_process(user_id integer)
+returns table (trip_id integer, current_trip_start_date bigint, 
+current_trip_end_date bigint, 
+previous_trip_end_date bigint, previous_trip_purpose integer, previous_trip_poi_name text, 
+next_trip_start_date bigint,
+purposes json) as 
+$b$
+with first_unprocessed_trip as (
+	select * from v2api.unprocessed_trips 
+	where user_id = $1
+	order by from_time, to_time 
+	limit 1), 
+	last_processed_trip as (
+	select * from v2api.processed_trips 
+	where user_id = $1 
+	order by from_time desc, to_time desc 
+	limit 1),
+	next_trip_to_process as (
+	select * from unprocessed_trips 
+	where user_id = $1 
+	and trip_id> (select trip_id from first_unprocessed_trip)
+	limit 1
+	)
+
+select first.trip_id, 
+	first.from_time as current_trip_start_date, first.to_time as current_trip_end_date, 
+	last.to_time as previous_trip_end_date, last.purpose_id as last_trip_purpose, 
+	(select name_ from poi_personal where gid = last.destination_poi_id
+	union select coalesce (name_,type_) from poi_public where osm_id = last.destination_poi_id
+	limit 1) as previous_trip_poi,
+	next.from_time as next_trip_start_date,
+	(select * from ap_get_purposes()) as purposes
+	 from first_unprocessed_trip first, last_processed_trip last,
+	 next_trip_to_process next 
+ $b$ 
+ LANGUAGE SQL;
+
 /*
 Annotated trips and triplegs annotation 
 */
@@ -29,6 +68,4 @@ create view v2api.processed_trips as
 -- view that will serve the annotated triplegs per trip
 create view v2api.processed_triplegs as 
 	select * from triplegs_inf where trip_id = 
-	any (select trip_id from v2api.processed_trips);
-
-	
+	any (select trip_id from v2api.processed_trips);	
---
+++ SQL/v2_raw.sql
@@ -0,0 +1,104 @@
+﻿/*
+** SCHEMA DEFINITION
+*/
+create schema if not exists raw_data; 
+comment on schema raw_data is
+'For raw measurements that are not modified (locations, user credentials)';
+
+/*
+** TABLE DEFINITIONS
+*/
+CREATE TABLE if not exists raw_data.location_table
+(
+  id serial NOT NULL,
+  upload boolean DEFAULT false,
+  accuracy_ double precision,
+  altitude_ double precision,
+  bearing_ double precision,
+  lat_ double precision,
+  lon_ double precision,
+  time_ bigint,
+  speed_ double precision,
+  satellites_ integer,
+  user_id integer,
+  size integer,
+  totalismoving boolean,
+  totalmax real,
+  totalmean real,
+  totalmin real,
+  totalnumberofpeaks integer,
+  totalnumberofsteps integer,
+  totalstddev real,
+  xismoving boolean,
+  xmaximum real,
+  xmean real,
+  xminimum real,
+  xnumberofpeaks integer,
+  xstddev real,
+  yismoving boolean,
+  ymax real,
+  ymean real,
+  ymin real,
+  ynumberofpeaks integer,
+  ystddev real,
+  zismoving boolean,
+  zmax real,
+  zmean real,
+  zmin real,
+  znumberofpeaks integer,
+  zstddev real,
+  provider text
+);
+
+Comment on table raw_data.location_table is 
+'stores the locations retrieved from the clients';
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS citext; 
+
+CREATE TABLE if not exists raw_data.user_table
+(
+  id serial NOT NULL,
+  username citext NOT NULL UNIQUE,
+  password text NOT NULL,
+  phone_model citext NOT NULL,
+  phone_os text,
+  CONSTRAINT user_table_pkey PRIMARY KEY (id)
+);
+
+comment on table raw_data.user_table is 
+'stores the credentials of the user, password is hashed (needs pgcrypto), citext is used for case insensitive storage of email addresses'; 
+
+/*
+** FUNCTION DEFINTIONS
+*/
+
+create or replace function raw_data.register_user(username citext, password text, phone_model text, phone_os text)
+returns integer as 
+$fct_body$
+	INSERT INTO raw_data.user_TABLE (username, password, phone_model, phone_os)
+	VALUES ($1, crypt($2, gen_salt('bf',8)), $3, $4)
+	RETURNING id;
+$fct_body$
+LANGUAGE SQL;
+
+comment on function raw_data.register_user(username citext, password text, phone_model text, phone_os text) is 
+'inserts a new user with hashed password and returns the user_id for api_end_point related to register_user';
+-- foo example 
+-- select register_user as id from raw_data.register_user('adi@kth.se', 'adi', 'android', 'android model');
+
+create or replace function raw_data.login_user(username citext, password text)
+returns integer as 
+$fct_body$
+	SELECT id from raw_data.user_TABLE 
+	where 
+	username = $1
+	and password = crypt($2, password);
+$fct_body$
+LANGUAGE SQL;
+
+comment on function raw_data.login_user(username citext, password text) is 
+'checks if the credentials are correct and returns the user_id for api_end_point related to login_user';
+
+-- foo example 
+-- select login_user as id from raw_data.login_user('adi@kth.se', 'adi');
